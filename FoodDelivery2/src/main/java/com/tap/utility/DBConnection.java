b8550a040966c096b3ba1ffe6c771beab0ad5683
Vinay0745 <112702656+Vinay0745@users.noreply.github.com>
2026-01-03 14:03:57 +0530

    Update DBConnection.java

--- FoodDelivery2/src/main/java/com/tap/utility/DBConnection.java
+++ FoodDelivery2/src/main/java/com/tap/utility/DBConnection.java
@@ -5,22 +5,22 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
+    private static final String HOST = System.getenv("MYSQLHOST");
+    private static final String PORT = System.getenv("MYSQLPORT");
+    private static final String DATABASE = System.getenv("MYSQLDATABASE");
+    private static final String USER = System.getenv("MYSQLUSER");
+    private static final String PASSWORD = System.getenv("MYSQLPASSWORD");
+
+    private static final String URL =
+        "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE +
+        "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
     public static Connection getConnection() {
         try {
-            String host = System.getenv("MYSQLHOST");
-            String port = System.getenv("MYSQLPORT");
-            String database = System.getenv("MYSQLDATABASE");
-            String user = System.getenv("MYSQLUSER");
-            String password = System.getenv("MYSQLPASSWORD");
-
-            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
-                    + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(url, user, password);
-
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace(); // This will show errors in Railway logs
             return null;
         }
     }
