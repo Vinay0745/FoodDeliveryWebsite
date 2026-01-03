2b11bd7e5189e0b18a02a82fefb4d5b2a88fa895
Vinay0745 <112702656+Vinay0745@users.noreply.github.com>
2026-01-03 12:52:23 +0530

    Update DBConnection.java

--- FoodDelivery2/src/main/java/com/tap/utility/DBConnection.java
+++ FoodDelivery2/src/main/java/com/tap/utility/DBConnection.java
@@ -2,25 +2,26 @@ package com.tap.utility;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnection {
-	private static final String URL = "jdbc:mysql://localhost:3306/food_delivery";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "vinay";
-	public static final Connection getConnection() {
-		Connection connection=null;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return connection;
-		
-	}
+
+    private static final String HOST = System.getenv("mysql.railway.internal");
+    private static final String PORT = System.getenv("3306");
+    private static final String DATABASE = System.getenv("railway");
+    private static final String USER = System.getenv("root");
+    private static final String PASSWORD = System.getenv("kNCDvtbSNQizejrkJwizJizVgfWydsGj");
+
+    private static final String URL =
+        "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE +
+        "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
